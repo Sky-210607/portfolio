387fe1d1f3cc95e728c6fcdcc3cb41eac33ca711
Sky-210607 <aritrikadhara@gmail.com>
2025-01-25 22:18:39 +0530

    Create app.jsx

---
+++ app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+        import Portfolio from './components/Portfolio'; // Update path accordingly
+
+        import './App.css'; // Or import your global CSS if needed
+
+        function App() {
+            return (
+                <div className='App'>
+                    <Portfolio />
+                </div>
+            );
+        }
+
+        export default App;
